ab31cf572fe6ac31eeba8ed65a925d10b85c4688
Reinaldo <baptistare81@hotmail.com>
2023-06-10 17:34:55 -0300

    change city size

--- Person/Tables/Address.sql
+++ Person/Tables/Address.sql
@@ -2,7 +2,7 @@
     [AddressID]       INT               IDENTITY (1, 1) NOT FOR REPLICATION NOT NULL,
     [AddressLine1]    NVARCHAR (60)     NOT NULL,
     [AddressLine2]    NVARCHAR (60)     NULL,
-    [City]            NVARCHAR (40)     NOT NULL,
+    [City]            NVARCHAR (30)     NOT NULL,
     [StateProvinceID] INT               NOT NULL,
     [PostalCode]      NVARCHAR (15)     NOT NULL,
     [SpatialLocation] [sys].[geography] NULL,
